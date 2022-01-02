4d5d6a7d6faaa339817ad1b571fd974f568b9cc1
UniqueSwagger <rahmanmahi02@gmail.com>
2022-01-02 23:41:02 +0600

    HeroSection added

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react";
 import Header from "../../../components/SharedComponents/Header/Header";
+import HeroSection from "../HeroSection/HeroSection";
 
 const Home = () => {
   return (
     <Fragment>
       <Header />
+      <HeroSection />
     </Fragment>
   );
 };
