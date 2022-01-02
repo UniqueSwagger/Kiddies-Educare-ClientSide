10e5efa066e0f2fc39b6113f7dd6cc15fafb75ab
jana-alam <janaalam2929@gmail.com>
2022-01-02 18:35:53 +0600

    Home page Features

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react";
+import Features from "../../../components/Features/Features";
 import Header from "../../../components/SharedComponents/Header/Header";
 
 const Home = () => {
   return (
     <Fragment>
       <Header />
+      <Features />
     </Fragment>
   );
 };
