3945e0ff6ad9a0f1a4cffdc303a3eeb4c9a7f942
UniqueSwagger <rahmanmahi02@gmail.com>
2022-01-03 15:21:27 +0600

    Added loader

--- src/pages/Home/Events/EventDetails/EventDetails.js
+++ src/pages/Home/Events/EventDetails/EventDetails.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { useParams } from "react-router-dom";
 import BreadCrumb from "../../../../components/BreadCrumb/BreadCrumb";
+import Loader from "../../../../components/Loader/Loader";
 import useEvents from "../../../../hooks/useEvents";
 const EventDetails = () => {
   const { id } = useParams();
   const events = useEvents();
   const event = events?.find((event) => event._id === id);
   return (
-    <div>
-      <BreadCrumb title={event?.title} />
-    </div>
+    <Fragment>
+      {event ? (
+        <div>
+          <BreadCrumb title={event?.title} />
+        </div>
+      ) : (
+        <Loader />
+      )}
+    </Fragment>
   );
 };
 
