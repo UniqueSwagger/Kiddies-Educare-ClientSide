e7875d6b2a30048d66fe5c26e6ecb70f48b27b8d
UniqueSwagger <rahmanmahi02@gmail.com>
2022-01-04 16:40:16 +0600

    added shop

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import allProductSlice from "./slices/allProductSlice";
 import eventsSlice from "./slices/eventsSlice";
 export const store = configureStore({
   reducer: {
     events: eventsSlice,
+    products: allProductSlice,
   },
 });
